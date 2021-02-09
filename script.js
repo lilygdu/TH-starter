d823a1f83db816ea17918b552175bc6cc127b263
Andy Weiss <andyweiss1982@gmail.com>
2021-02-09 09:33:43 -0500

    validate on keyup after submission

--- script.js
+++ script.js
@@ -1,65 +1,44 @@
 const form = document.querySelector("#form");
+const inputs = form.querySelectorAll("input");
+let submitted = false;
 
 function handleSubmit(event) {
   event.preventDefault();
-
-  const inputs = form.querySelectorAll("input");
-
+  submitted = true;
   inputs.forEach((input) => {
-    if (input.checkValidity()) {
-      if (input.type === "text" || input.type === "email") {
-        console.log("It is valid- text, email");
-        input.classList.add("valid");
-        input.classList.remove("invalid");
-
-        input
-          .closest(".form-field")
-          .querySelector(".error-message").textContent = "";
-      } else if (input.name === "promotional-emails") {
-        console.log("first checkbox");
-      } else {
-        console.log("It is valid- checkbox");
-
-        input
-          .closest(".checkbox-field")
-          .querySelector(".error-message").textContent = "";
-      }
-    } else {
-      console.log("not valid");
-      input.classList.add("invalid");
-      input.classList.remove("valid");
-
-      if (input.type === "email") {
-        if (input.value === "") {
-          input
-            .closest(".form-field")
-            .querySelector(".error-message").textContent =
-            "Email is a required field.";
-        } else {
-          input
-            .closest(".form-field")
-            .querySelector(".error-message").textContent =
-            "That doesn't look like a valid email.";
-        }
-      }
-
-      if (input.type === "text") {
-        input
-          .closest(".form-field")
-          .querySelector(".error-message").textContent =
-          "Name is a required field.";
-      }
-
-      if ((input.type === "checkbox") & (input.id === "terms-of-service")) {
-        console.log("checkbox type");
-
-        input
-          .closest(".checkbox-field")
-          .querySelector(".error-message").textContent =
-          "You must agree to the privacy policy and terms of service before signing up.";
-      }
-    }
+    validate(input);
   });
 }
 
+function validate(input) {
+  if (!submitted) return;
+  let errorMessage = "";
+  if (input.checkValidity()) {
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    if (input.type === "email") {
+      if (input.value === "") {
+        errorMessage = "Email is a required field.";
+      } else {
+        errorMessage = "That doesn't look like a valid email.";
+      }
+    }
+    if (input.type === "text") {
+      errorMessage = "Name is a required field.";
+    }
+    if (input.type === "checkbox") {
+      errorMessage =
+        "You must agree to the privacy policy and terms of service before signing up.";
+    }
+  }
+  input
+    .closest(".form-field")
+    .querySelector(".error-message").textContent = errorMessage;
+}
+
 form.addEventListener("submit", handleSubmit);
+
+inputs.forEach((input) =>
+  input.addEventListener("input", () => validate(input))
+);
