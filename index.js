1600c81f7dd129c33e0cf05c5097fa27276b5a5f
Andy Weiss <andyweiss1982@gmail.com>
2021-02-23 11:26:26 -0500

    stitch it all back together

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import db from "./db.js";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 app.use(express.static("public"));
