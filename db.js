bfae82ba76dc93803168676fc75efbc6bfa35c00
Andy Weiss <andyweiss1982@gmail.com>
2021-02-23 11:49:36 -0500

    align table with th data

--- db.js
+++ db.js
@@ -4,10 +4,14 @@ const pool = new pg.Pool();
 
 pool.query(`
   CREATE TABLE IF NOT EXISTS th_users(                                       
+    id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
     email VARCHAR(256) UNIQUE NOT NULL,
-    password VARCHAR(256) NOT NULL CHECK (length(password) >= 8),
-    id uuid DEFAULT uuid_generate_v4(),
-    PRIMARY KEY (id)
+    otp CHAR(6) NOT NULL,
+    country VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    email_consent BOOLEAN NOT NULL,
+    tos_consent BOOLEAN NOT NULL,
+    dob DATE
   );
 `);
 
