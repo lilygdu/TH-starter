cf093e5937897b7f7093d3f6bfaad92baef4e4e5
Bugbey <rbieb001@BKUSL-44B8ML85.attlocal.net>
2021-03-04 11:54:36 -0500

    header element

---
+++ public/javascript/demo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "../components/Header";
+
+const PageDemo = () => {
+  return <Header />;
+};
+
+ReactDOM.render(<PageDemo />, document.querySelector("#root"));
