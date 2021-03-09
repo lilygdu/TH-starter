916519645828a3a9cc56d9dac4e80cf807387797
Andy Weiss <andyweiss1982@gmail.com>
2021-03-09 11:35:43 -0500

    item document with relationship and validations

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -12,27 +12,57 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     {
-      // This is the display name for the type
       title: "Category",
-      // The identifier for this document type used in the api's
       name: "category",
-      // Documents have the type 'document'. Your schema may describe types beyond documents
-      // but let's get back to that later.
       type: "document",
-      // Now we proceed to list the fields of our document
       fields: [
         {
-          // The display name for this field
           title: "Name",
-          // The identifier for this field used in the api's
           name: "name",
-          // The type of this field
           type: "string",
+          validation: (Rule) => Rule.required(),
         },
         {
           title: "Primary Image",
           name: "primaryImage",
           type: "image",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Items",
+          name: "items",
+          type: "array",
+          of: [{ type: "reference", to: [{ type: "item" }] }],
+        },
+      ],
+    },
+    {
+      title: "Item",
+      name: "item",
+      type: "document",
+      fields: [
+        {
+          title: "Name",
+          name: "name",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Primary Image",
+          name: "primaryImage",
+          type: "image",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Price",
+          name: "price",
+          type: "number",
+          description: "An integer number of cents",
+          validation: (Rule) =>
+            Rule.required()
+              .min(0)
+              .integer()
+              .error("Items must have a price of at least 0 cents"),
         },
       ],
     },
